1934f5115800184ed3f03b05f7a9e68a00b46228
Ashley <ashleyriederman@gmail.com>
2019-01-15 15:55:53 -0600

    Updated player behavior

---
+++ Assets/Scripts/Actors/ActorMB.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public abstract class ActorMB : MonoBehaviour
+{
+    protected NodeMB currentNode;
+
+    public DirectionType CurrentDirection { get; private set; }
+
+    public void Initialize(NodeMB currentNode)
+    {
+        // Initialize references
+        this.currentNode = currentNode;
+        Assert.IsTrue(currentNode != null);
+
+        // Set the starting position
+        transform.position = currentNode.transform.position;
+
+        Debug.Log("Initialize " + name);
+    }
+
+    public void Move(DirectionType direction)
+    {
+        // Get the node to move to
+        NodeMB targetNode = currentNode.GetNextNode(direction);
+
+        // Set the new position
+        if (targetNode != null)
+        {
+            currentNode = targetNode;
+            transform.position = targetNode.transform.position;
+        }
+
+        // Set facing direction
+        CurrentDirection = direction;
+    }
+}
---
+++ Assets/Scripts/Actors/PlayerMB.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMB : ActorMB
+{
+    private void Update()
+    {
+        // Check for movement input
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(DirectionType.Left);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(DirectionType.Right);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(DirectionType.Down);
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(DirectionType.Up);
+        }
+    }
+}
